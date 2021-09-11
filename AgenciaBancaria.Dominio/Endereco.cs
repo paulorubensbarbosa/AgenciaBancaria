3359b6775450b6d58d57aefffaeaa2d821741b82
Paulo Rubens <paulorubensbarbosa@outlook.com>
2021-09-11 10:41:52 -0300

    Initial Commit

---
+++ AgenciaBancaria.Dominio/Endereco.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgenciaBancaria.Dominio
+{
+    public class Endereco
+    {
+        public Endereco(string logradouro, string cep, string cidade, string estado)
+        {
+            this.Logradouro = logradouro;
+            this.CEP = cep;
+            this.Cidade = cidade;
+            this.Estado = estado;
+        }
+        public string Logradouro { get; private set; }
+        public string CEP { get; private set; }
+        public string Cidade { get; private set; }
+        public string Estado { get; private set; }
+    }
+}
